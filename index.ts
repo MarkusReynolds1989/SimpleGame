200cd5fa3c472e736130a74b0fccd438c9af35b7
Markus <Markusreynolds1989@gmail.com>
2020-10-02 13:00:27 -0500

    Recreated the project in typescript

--- index.ts
+++ index.ts
@@ -1,31 +1,39 @@
-"use strict";
-const canvas = document.querySelector("#canvas"), gl = canvas.getContext("webgl"), screenWidth = canvas.width, screenHeight = canvas.height, halfScreenWidth = canvas.width / 2, halfScreenHeight = canvas.height / 2;
+const canvas = <HTMLCanvasElement>document.querySelector("#canvas")
+    , gl = canvas.getContext("webgl")
+    , screenWidth = canvas.width
+    , screenHeight = canvas.height
+    , halfScreenWidth = canvas.width / 2
+    , halfScreenHeight = canvas.height / 2;
+
 // check gl.
 if (gl == null) {
     throw "GL Assignment failed";
 }
+
 // Step 1, create graphics objects and shader program object.
-let vertexBuffer = gl.createBuffer();
-let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-let shaderProgram = gl.createProgram();
-if (shaderProgram == null) {
+let vertexBuffer: WebGLBuffer | null = gl.createBuffer();
+let vertexShader: WebGLShader | null = gl.createShader(gl.VERTEX_SHADER);
+let fragmentShader: WebGLShader | null = gl.createShader(gl.FRAGMENT_SHADER);
+let shaderProgram: WebGLProgram | null = gl.createProgram();
+
+if(shaderProgram == null) {
     throw "shaderProgram failed";
 }
 // Step 2, set code for shaders.
-let vertCode = `
+let vertCode: string = `
         attribute vec2 coordinates;
         void main(void) 
         {
             gl_Position = vec4(coordinates, 0.0, 1.0);
             gl_PointSize = 1.0;
         }`;
-let fragCode = `
+let fragCode: string = `
         void main(void)
         { 
             // Red green blue alpha
             gl_FragColor = vec4(100.0,0.0,0.0, 0.5);
         }`;
+
 // Step3 Compile.
 // For the fragment shader.
 if (fragmentShader != null) {
@@ -38,10 +46,10 @@ if (vertexShader != null) {
     gl.shaderSource(vertexShader, vertCode);
     gl.compileShader(vertexShader);
     console.log(gl.getShaderInfoLog(vertexShader));
-}
-else {
+} else {
     throw "vertexShader null";
 }
+
 // Step4 Attach and link.
 if (shaderProgram != null
     && vertexShader != null
@@ -52,13 +60,14 @@ if (shaderProgram != null
     console.log(gl.getProgramInfoLog(shaderProgram));
     gl.useProgram(shaderProgram);
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-}
-else {
+} else {
     throw "ShaderProgram invaild";
 }
+
 // Step5 bind
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-let coord;
+
+let coord: number;
 // Other
 if (shaderProgram != null) {
     coord = gl.getAttribLocation(shaderProgram, "coordinates");
@@ -66,51 +75,59 @@ if (shaderProgram != null) {
     gl.enableVertexAttribArray(coord);
     gl.viewport(0, 0, canvas.width, canvas.height);
 }
-const glDraw = (vertBuffer, array, type, length) => {
+
+const glDraw = (vertBuffer: WebGLBuffer | null
+    , array: number[]
+    , type: number
+    , length: number): void => {
     gl.bindBuffer(gl.ARRAY_BUFFER, vertBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(array), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     gl.drawArrays(type, 0, length);
 };
-const translateX = (num) => {
+
+const translateX = (num: number): number => {
     if (num == 0) {
         return -1;
-    }
-    else if (num == screenWidth) {
+    } else if (num == screenWidth) {
         return 1;
-    }
-    else if (num == halfScreenWidth) {
-        return 0;
-    }
-    else {
+    } else if (num == halfScreenWidth) {
+        return 0
+    } else {
         return num / halfScreenWidth - 1;
     }
 };
-const translateY = (num) => {
+
+const translateY = (num: number): number => {
     if (num == 0) {
         return 1;
-    }
-    else if (num == screenHeight) {
+    } else if (num == screenHeight) {
         return -1;
-    }
-    else if (num == halfScreenHeight) {
+    } else if (num == halfScreenHeight) {
         return 0;
-    }
-    else {
-        return num / halfScreenHeight * -1 + 1;
+    } else {
+        return num / halfScreenHeight * - 1 + 1;
     }
 };
-const drawLine = (x, y, endX, endY) => {
+
+const drawLine = (x: number
+    , y: number
+    , endX: number
+    , endY: number) => {
     const line = [
-        translateX(x),
-        translateY(y),
-        translateX(endX),
-        translateY(endY)
+        translateX(x)
+        , translateY(y)
+        , translateX(endX)
+        , translateY(endY)
     ];
     glDraw(vertexBuffer, line, gl.LINES, line.length);
 };
+
 // Incorrect because too small.
-const drawRectangleLines = (x, y, width, height) => {
+const drawRectangleLines = (x: number
+    , y: number
+    , width: number
+    , height: number) => {
     x = translateX(x);
     width /= halfScreenHeight;
     y = translateY(y);
@@ -120,30 +137,38 @@ const drawRectangleLines = (x, y, width, height) => {
     // correct.
     // Could also jsut draw a rectangle and erase the
     // middle.
-    const lines = [
+    const lines: number[] = [
         x, y,
+        x + width + 0.015, y, // To the right
         x + width + 0.015, y,
-        x + width + 0.015, y,
+        x + width + 0.015, y - height - 0.23, // To the bottom
         x + width + 0.015, y - height - 0.23,
-        x + width + 0.015, y - height - 0.23,
+        x, y - height - 0.23, // To the bottom and
         x, y - height - 0.23,
-        x, y - height - 0.23,
-        x, y,
+        x, y, // To the top
     ];
+
     glDraw(vertexBuffer, lines, gl.LINES, lines.length);
 };
-const drawRectangle = (x, y, width, height) => {
+
+const drawRectangle = (x: number
+    , y: number
+    , width: number
+    , height: number) => {
     let points = []; // Array holding all of the points we will draw.
     x = translateX(x);
     y = translateY(y);
     width /= halfScreenHeight;
     height /= halfScreenWidth;
+
     // TODO: Handle negative x and y.
     for (let xPos = x; xPos < width + x; xPos += 0.001) {
         for (let yPos = y; yPos > y - height; yPos -= 0.001) {
             points.push(xPos, yPos);
         }
     }
+
     glDraw(vertexBuffer, points, gl.POINTS, points.length);
 };
+
 drawRectangle(100, 100, 500, 500);
